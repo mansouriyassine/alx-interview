2a3f20c826e6bc8ee7ef259fc0e1d5b3d9469407
mansouriyassine <mansouri.yassine1@gmail.com>
2024-04-16 00:04:00 +0100

    0

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+from collections import deque
+
+def canUnlockAll(boxes):
+    if not boxes:
+        return False
+
+    n = len(boxes)
+    visited = set()
+    queue = deque([0])
+
+    while queue:
+        current_box = queue.popleft()
+        visited.add(current_box)
+
+        for key in boxes[current_box]:
+            if key < n and key not in visited:
+                queue.append(key)
+
+    return len(visited) == n
