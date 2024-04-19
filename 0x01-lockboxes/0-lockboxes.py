ccb3730e8cd37349f2175b18feac4f0b5aefa831
mansouriyassine <mansouri.yassine1@gmail.com>
2024-04-19 19:18:15 +0100

    0

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,26 +1,41 @@
 #!/usr/bin/python3
+'''
+0-lockboxes.py
+'''
+
 
 def canUnlockAll(boxes):
-    """
-    Determine if all boxes can be opened.
+    '''
+    Check if it's possible to unlock all boxes.
 
     Args:
-        boxes (list): A list of lists representing the locked boxes.
-                      Each inner list contains the keys present in that box.
+        boxes (list): A list of lists representing locked boxes.
+                      Each inner list contains keys present in that box.
 
     Returns:
         bool: True if all boxes can be opened, False otherwise.
-    """
-    if not boxes:
-        return False
+    '''
+    # get the number of boxes
+    n = len(boxes)
 
-    keys = set(boxes[0])
-    visited = set([0])
+    # initialize an array to mark if a box has been visited or not
+    visited = [False] * n
 
-    while keys:
-        key = keys.pop()
-        if key < len(boxes):
-            visited.add(key)
-            keys.update(set(boxes[key]) - visited)
+    # mark the first box as visited
+    visited[0] = True
 
-    return len(visited) == len(boxes)
+    # initialize a list to keep track of keys found in boxes
+    found_keys = [0]
+
+    # loop through the list of found keys and mark all the boxes that can be
+    # opened by each key as visited
+    while found_keys:
+        box_idx = found_keys.pop()
+        boxes_to_open = boxes[box_idx]
+        for key in boxes_to_open:
+            if key < n and not visited[key]:
+                visited[key] = True
+                found_keys.append(key)
+
+    # check if all boxes have been visited
+    return all(visited)
