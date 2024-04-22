03d469f38f85c0922455afd678e0a6ab716aaf8e
mansouriyassine <mansouri.yassine1@gmail.com>
2024-04-22 08:35:22 +0100

    0

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -4,6 +4,7 @@ Calculates the minimum number of operations needed
 to achieve a given number of characters
 """
 
+
 def minOperations(n):
     """
     Calculates the fewest number of operations needed to
@@ -11,15 +12,14 @@ def minOperations(n):
     """
     if n <= 1:
         return 0
-    
+
     dp = [0] * (n + 1)
-    
+
     for i in range(2, n + 1):
         dp[i] = i
-        
+
         for j in range(2, int(i**0.5) + 1):
             if i % j == 0:
                 dp[i] = min(dp[i], dp[j] + i // j)
                 dp[i] = min(dp[i], dp[i // j] + j)
-
     return dp[n]
