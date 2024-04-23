a192f800751e00f737368921ff9be34d4454c3e4
mansouriyassine <mansouri.yassine1@gmail.com>
2024-04-23 14:17:08 +0100

    0

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -6,19 +6,19 @@ def minOperations(n):
     '''Calculates the fewest number of
     operations needed to result in exactly n H
     characters in this file.
-    
+
     Args:
         n (int): Target number of characters
-        
+
     Returns:
         int: Minimum number of operations
-        
+
     Note:
         If n is impossible to achieve, return 0
     '''
     if n <= 1:
         return 0
-    
+
     clipboard = 1
     pasted_chars = 1
     operations = 0
