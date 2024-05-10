2c0fee0972739d2b4b42e0dc79d72d1a276c0968
mansouriyassine <mansouri.yassine1@gmail.com>
2024-05-10 18:22:24 +0100

    0

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,37 +1,52 @@
 #!/usr/bin/python3
+
 """
-Module for UTF-8 Validation
+A script to determine if a given data set represents a valid UTF-8 encoding.
 """
+
 
 def validUTF8(data):
     """
-    Determines if a given data set represents a valid UTF-8 encoding.
+    Determine if a given data set represents a valid UTF-8 encoding.
 
     Args:
-        data (list): A list of integers representing byte data.
+        data (list[int]): A list of integers representing bytes of data.
 
     Returns:
-        bool: True if data is a valid UTF-8 encoding, else False.
+        bool: True if data is a valid UTF-8 encoding, False otherwise.
     """
-    def check_bytes(num_bytes):
-        """
-        Check if the next 'num_bytes' bytes in 'data' form a valid UTF-8 character.
-        """
-        for i in range(index + 1, index + num_bytes + 1):
-            if i >= len(data) or (data[i] >> 6) != 0b10:
-                return False
-        return True
 
-    index = 0
-    while index < len(data):
-        if (data[index] >> 7) == 0b0:
-            index += 1
-        elif (data[index] >> 5) == 0b110 and check_bytes(1):
-            index += 2
-        elif (data[index] >> 4) == 0b1110 and check_bytes(2):
-            index += 3
-        elif (data[index] >> 3) == 0b11110 and check_bytes(3):
-            index += 4
+    def is_start_byte(byte):
+        """Check if the given byte is a valid start byte."""
+        return (byte >> 7) == 0b0 or \
+               (byte >> 5) == 0b110 or \
+               (byte >> 4) == 0b1110 or \
+               (byte >> 3) == 0b11110
+
+    def is_continuation_byte(byte):
+        """Check if the given byte is a continuation byte."""
+        return (byte >> 6) == 0b10
+
+    i = 0
+    while i < len(data):
+        byte = data[i]
+        if not is_start_byte(byte):
+            return False
+        num_bytes = 0
+        if (byte >> 7) == 0b0:
+            num_bytes = 1
+        elif (byte >> 5) == 0b110:
+            num_bytes = 2
+        elif (byte >> 4) == 0b1110:
+            num_bytes = 3
+        elif (byte >> 3) == 0b11110:
+            num_bytes = 4
         else:
             return False
+        if i + num_bytes > len(data):
+            return False
+        for j in range(i + 1, i + num_bytes):
+            if not is_continuation_byte(data[j]):
+                return False
+        i += num_bytes
     return True
