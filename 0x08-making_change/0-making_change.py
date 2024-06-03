7eb52f3dfb102b2bc142fdb902ab46cc5de55095
mansouriyassine <mansouri.yassine1@gmail.com>
2024-06-03 17:36:21 +0100

    0

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -27,6 +27,7 @@ Assumptions:
 Your solution’s runtime will be evaluated in this task.
 """
 
+
 def makeChange(coins, total):
     """
     Calculate the fewest number of coins needed to meet the given total
@@ -46,14 +47,14 @@ def makeChange(coins, total):
     """
     if total < 0:
         return -1
-    
+
     min_coins = [float('inf')] * (total + 1)
     min_coins[0] = 0
-    
+
     for amount in range(1, total + 1):
         for coin in coins:
             if coin <= amount:
                 min_coins[amount] = min(min_coins[amount],
                                         min_coins[amount - coin] + 1)
-    
+
     return min_coins[total] if min_coins[total] != float('inf') else -1
