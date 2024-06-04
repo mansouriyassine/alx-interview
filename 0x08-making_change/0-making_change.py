ba6cd7f07de995bda0b18fcf2e4a5e8151cbe32f
mansouriyassine <mansouri.yassine1@gmail.com>
2024-06-04 14:29:23 +0100

    01

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -47,12 +47,16 @@ def makeChange(coins, total):
     """
     if total <= 0:
         return 0
-    
+
     dp = [float('inf')] * (total + 1)
     dp[0] = 0
-    
-    for coin in coins:
-        for i in range(coin, total + 1):
-            dp[i] = min(dp[i], dp[i - coin] + 1)
-    
-    return dp[total] if dp[total] != float('inf') else -1
+
+    for i in range(1, total + 1):
+        for coin in coins:
+            if coin <= i:
+                dp[i] = min(dp[i], dp[i - coin] + 1)
+
+    if dp[total] == float('inf'):
+        return -1
+    else:
+        return dp[total]
