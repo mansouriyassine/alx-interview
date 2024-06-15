1c923763d5af7422f88e359839cb0cdcdb862c35
mansouriyassine <mansouri.yassine1@gmail.com>
2024-06-15 17:40:45 +0100

    0

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -42,13 +42,14 @@ Directory: 0x0A-primegame
 File: 0-prime_game.py
 """
 
+
 def sieve_of_eratosthenes(max_n):
     """
     Sieve of Eratosthenes algorithm to find all prime numbers up to max_n.
-    
+
     Parameters:
     - max_n: Maximum number up to which prime numbers are found.
-    
+
     Returns:
     - List of prime numbers up to max_n.
     """
@@ -62,23 +63,24 @@ def sieve_of_eratosthenes(max_n):
     prime_numbers = [p for p in range(2, max_n + 1) if is_prime[p]]
     return prime_numbers
 
+
 def isWinner(x, nums):
     """
     Determine the winner of each round of the prime game.
-    
+
     Parameters:
     - x: Number of rounds.
     - nums: List of integers n for each round.
-    
+
     Returns:
     - Name of the player that won the most rounds ('Maria' or 'Ben').
       If the winner cannot be determined, return None.
     """
     max_n = max(nums)
     prime_numbers = sieve_of_eratosthenes(max_n)
-    
+
     dp = [False] * (max_n + 1)
-    
+
     for i in range(2, max_n + 1):
         dp[i] = False
         for prime in prime_numbers:
@@ -87,15 +89,15 @@ def isWinner(x, nums):
             if not dp[i - prime]:
                 dp[i] = True
                 break
-    
+
     wins = {"Maria": 0, "Ben": 0}
-    
+
     for n in nums:
         if dp[n]:
             wins["Maria"] += 1
         else:
             wins["Ben"] += 1
-    
+
     if wins["Maria"] > wins["Ben"]:
         return "Maria"
     elif wins["Ben"] > wins["Maria"]:
