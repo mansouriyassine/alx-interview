2d75a83d2ec18c1597e2880172fe2fb0a4ec7714
mansouriyassine <mansouri.yassine1@gmail.com>
2024-07-12 12:33:42 +0100

    0

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -5,6 +5,7 @@ Given a pile of coins of different values, determine the fewest number
 of coins needed to meet a given amount total.
 """
 
+
 def makeChange(coins, total):
     """
     Calculate the fewest number of coins needed to meet the given total
@@ -12,16 +13,16 @@ def makeChange(coins, total):
     """
     if total <= 0:
         return 0
-    
+
     coins.sort(reverse=True)
     count = 0
-    
+
     for coin in coins:
         while total >= coin:
             total -= coin
             count += 1
-        
+
         if total == 0:
             return count
-    
+
     return -1
